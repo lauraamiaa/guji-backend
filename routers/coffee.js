c31bb5e3d6a52880debc0d55cf21a4173652beec
lauraamiaa <miatkowski.laura@gmail.com>
2021-10-13 14:42:11 +0200

    created find by id coffee route

--- routers/coffee.js
+++ routers/coffee.js
@@ -5,10 +5,26 @@ const Coffee = require("../models").coffee;
 
 const router = new Router();
 
+// get all coffees
 router.get("/", async (req, res, next) => {
   try {
     const allCoffees = await Coffee.findAll();
     res.send(allCoffees);
+  } catch (e) {
+    next(e);
+  }
+});
+
+// get one coffee by id
+router.get("/:id", async (req, res, next) => {
+  try {
+    const coffeeId = parseInt(req.params.id);
+    const coffee = await Coffee.findByPk(coffeeId);
+    if (coffee) {
+      res.send(coffee);
+    } else {
+      res.status(404).send("Product not found");
+    }
   } catch (e) {
     next(e);
   }
